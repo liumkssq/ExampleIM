8cd2b6aeeacd8f0e14319337445be55ff8a8efef
Daivd Malan <129161685+liumkssq@users.noreply.github.com>
2025-03-09 14:34:58 +0800

    feat(im): 添加 IM 消息服务 API
    - 新增配置文件和主要的 service context
    - 实现了获取聊天记录、获取会话、更新会话和建立会话的 handler 和 logic
    - 添加了 Swagger JSON 文件用于 API 文档

---
+++ apps/im/api/internal/logic/getConversationsLogic.go
@@ -0,0 +1,41 @@
+package logic
+
+import (
+	"context"
+	"github.com/jinzhu/copier"
+	"github.com/liumkssq/easy-chat/apps/im/rpc/imclient"
+	"github.com/liumkssq/easy-chat/pkg/ctxdata"
+
+	"github.com/liumkssq/easy-chat/apps/im/api/internal/svc"
+	"github.com/liumkssq/easy-chat/apps/im/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetConversationsLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+// 获取会话
+func NewGetConversationsLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetConversationsLogic {
+	return &GetConversationsLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *GetConversationsLogic) GetConversations(req *types.GetConversationsReq) (resp *types.GetConversationsResp, err error) {
+	uid := ctxdata.GetUId(l.ctx)
+	data, err := l.svcCtx.GetConversations(l.ctx, &imclient.GetConversationsReq{
+		UserId: uid,
+	})
+	if err != nil {
+		return nil, err
+	}
+	var res types.GetConversationsResp
+	copier.Copy(&res, data)
+	return &res, nil
+}
---
+++ apps/im/api/internal/logic/putConversationsLogic.go
@@ -0,0 +1,39 @@
+package logic
+
+import (
+	"context"
+	"github.com/jinzhu/copier"
+	"github.com/liumkssq/easy-chat/apps/im/rpc/imclient"
+	"github.com/liumkssq/easy-chat/pkg/ctxdata"
+
+	"github.com/liumkssq/easy-chat/apps/im/api/internal/svc"
+	"github.com/liumkssq/easy-chat/apps/im/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type PutConversationsLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+// 更新会话
+func NewPutConversationsLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PutConversationsLogic {
+	return &PutConversationsLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *PutConversationsLogic) PutConversations(req *types.PutConversationsReq) (resp *types.PutConversationsResp, err error) {
+	uId := ctxdata.GetUId(l.ctx)
+	var conversationList map[string]*imclient.Conversation
+	copier.Copy(&conversationList, req.ConversationList)
+	_, err = l.svcCtx.PutConversations(l.ctx, &imclient.PutConversationsReq{
+		UserId:           uId,
+		ConversationList: conversationList,
+	})
+	return
+}
